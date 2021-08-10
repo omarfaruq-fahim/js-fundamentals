a0e817f34b4fb222c8eb13fe1dfa1ff8cb5c0c9d
omarfaruq-fahim <omar15-2270@diu.edu.bd>
2021-08-11 01:54:01 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [21, 23, 22];
+
+var muriChanacurFee = [34, 55, 222, 3];
+
+var nayikas = ['mahi', 'opu', 'pori', 'tisha'];
+
+console.log(nayikas.length);
---
+++ conditional.js
@@ -0,0 +1,24 @@
+// var isFoodReady = true;
+// if(isFoodReady == true){
+//     console.log('Mama!! I will eat now.')
+// }
+
+// var iphonePrice = 70000;
+// var myBudget = 19500
+
+// if(iphonePrice < myBudget){
+//     console.log('Iphone diya pic tule futani marbo')
+// }
+// if(iphonePrice > myBudget){
+//     console.log('My mi is the best')
+// }
+
+var chickenPrice = 180;
+var myMoney = 850;
+
+if(chickenPrice < myMoney){
+    console.log('Yes! murgir Ran khamu ar haddi chabamu')
+}
+else{
+    console.log('Smashed potato with Lentil soup')
+}
